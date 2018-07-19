6be38066fbbe996f76926cff3bbb2faf183c4867
shengxiluo <chi18765928341@163.com>
2018-07-19 12:03:19 +0800

    Initial commit

---
+++ src/main/java/com/join/hellogradle/HelloGradleApplication.java
@@ -0,0 +1,12 @@
+package com.join.hellogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGradleApplication.class, args);
+	}
+}
